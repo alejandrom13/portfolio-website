49be0cd418553a6abbb9b213b85010396c1058d9
Alejandro M <alejandromatos1202@gmail.com>
2022-01-27 15:15:37 -0400

    update

--- api/index.php
+++ api/index.php
@@ -2,3 +2,17 @@
 
 header('content-type: application/json');
 echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="">create text</a>
+</body>
+</html>
