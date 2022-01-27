06598b1129ae4b32f57cfc188596b084377fca72
Alejandro M <alejandromatos1202@gmail.com>
2022-01-27 15:26:21 -0400

    wordpress

--- api/index.php
+++ api/index.php
@@ -1,22 +1,3 @@
 <?php
 
-header('content-type: application/json');
-echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']);
-
-echo '
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <a href="">create tex</a>
-</body>
-</html>
-
-'
-?>
+require __DIR__ . '/../index.php';
